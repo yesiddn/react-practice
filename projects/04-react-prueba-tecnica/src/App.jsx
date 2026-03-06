20549d6b9b06d88c19ded1fb40e2b4a6b7e529d0
Duvan Yesid <yesidrodriguez305@gmail.com>
2026-03-05 21:47:19 -0500

    refactor: clean up App component by removing unused state and effects, and utilizing custom hooks

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,16 +1,20 @@
-import { useEffect, useState } from "react"
-import { getRandomFact } from "./services/facts"
+// import { useEffect, useState } from "react"
+// import { getRandomFact } from "./services/facts"
+import { useCatFact } from "./hooks/useCatFact"
 import { useCatImage } from "./hooks/useCatImage"
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 const CAT_ENDPOINT_IMAGE_URL = (firstWord) => `https://cataas.com/cat/says/${firstWord}?size=50&json=true`
 
+
 export function App() {
-  const [fact, setFact] = useState(null)
+  // const [fact, setFact] = useState(null)
+  const { fact, refreshRandomFact } = useCatFact()
   // const [imageUrl, setImageUrl] = useState(null)
+  // hay que evitar los nombres que aten el hook a la implementacion, por ejemplo, useFetchCatImage, porque si en un futuro queremos usar otra logica para obtener la imagen, el nombre del hook ya no tendría sentido
   const { imageUrl } = useCatImage({ fact })
 
-  // un solo effect
+  // un solo effect con mucha logica dentro no es recomendable, es mejor dividirlo en varios effects
   // useEffect(() => { 
   //   fetch(CAT_ENDPOINT_RANDOM_FACT)
   //     .then(response => response.json())
@@ -29,26 +33,27 @@ export function App() {
   //     })
   // }, [])
 
-  useEffect(() => { 
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(response => {
-        if (!response.ok) throw new Error('Network response was not ok')
+  // useEffect(() => { 
+  //   getRandomFact().then(newFact => setFact(newFact))
+  //   fetch(CAT_ENDPOINT_RANDOM_FACT)
+  //     .then(response => {
+  //       if (!response.ok) throw new Error('Network response was not ok')
 
-        return response.json()
-      })
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-      })
-      .catch(error => {
-        // al mandar un thow entra el error en el catch
-        // error con la respuesta
-        // error con la peticion
-        console.error('There has been a problem with your fetch operation:', error)
-      })
-  }, [])
+  //       return response.json()
+  //     })
+  //     .then(data => {
+  //       const { fact } = data
+  //       setFact(fact)
+  //     })
+  //     .catch(error => {
+  //       // al mandar un thow entra el error en el catch
+  //       // error con la respuesta
+  //       // error con la peticion
+  //       console.error('There has been a problem with your fetch operation:', error)
+  //     })
+  // }, [])
 
-  // se reemplaza por el custom hook
+  // se reemplaza por el custom hook useCatImage
   // useEffect(() => {
   //   if (!fact) return
 
@@ -62,10 +67,13 @@ export function App() {
   // }, [fact])
 
   const handleClick = async () => {
-    const newFact = await getRandomFact()
+    // const newFact = await getRandomFact()
     // nunca debe salir del componente el setStatement
-    setFact(newFact)
+    // setFact(newFact)
+    refreshRandomFact()
   }
+
+  // con todos los custum hooks, el componente queda muy limpio, y se puede reutilizar la logica de cada hook en otros componentes sin preocuparse por la implementacion interna de cada uno
   
   return (
     <main>
