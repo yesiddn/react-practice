0e2cfa25aa0529aae8c21b40e62d3143579c42ac
Duvan Yesid <yesidrodriguez305@gmail.com>
2026-03-10 22:00:33 -0500

    feat: refactor useMovies hook to utilize searchMovies service for fetching movie data

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,17 +1,53 @@
 
-import responseMovies from '../mocks/with-results.json';
-import responseNoMovies from '../mocks/no-results.json';
+import { useState } from 'react';
+import { searchMovies } from '../services/movies';
 
-export function useMovies() {
-  const movies = responseMovies.Search;
+export function useMovies({ query }) {
+  // una vez se mueve la logica a un servicio que obtiene y mapea los datos, el custom hook se vuelve mucho mas simple
+  // const [responseMovies, setResponseMovies] = useState([]);
+
+  // const movies = responseMovies.Search;
 
   // de esto se hablo en el componente Movies.jsx, es mejor hacer un mapeo de las propiedades que vienen de la API a unas propiedades mas amigables para nuestro código
-  const mappedMovies = movies?.map((movie) => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster,
-  }));
+  // const mappedMovies = movies?.map((movie) => ({
+  //   id: movie.imdbID,
+  //   title: movie.Title,
+  //   year: movie.Year,
+  //   poster: movie.Poster,
+  // }));
 
-  return { movies: mappedMovies };
+  // const getMovies = () => { 
+  //   if (query) {
+  //     // setResponseMovies(withResults);
+  //     fetch(`https://www.omdbapi.com/?apikey=e67d5036&s=${query}`)
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         setResponseMovies(data);
+  //       });
+  //   } else {
+  //     setResponseMovies(withoutResult);
+  //   }
+  // }
+  
+  // resultado:
+  const [movies, setMovies] = useState([]);
+  // incluso se puede mejorar
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const getMovies = async () => { 
+    setLoading(true);
+    setError(null);
+
+    try {
+      const newMovies = await searchMovies(query);
+      setMovies(newMovies);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { movies, getMovies, loading, error };
 }
